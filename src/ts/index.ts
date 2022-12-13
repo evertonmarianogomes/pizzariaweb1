312523e809aee527e70596f5537bd79276a13656
Everton M. Gomes <everton.mgomes123@gmail.com>
2022-12-13 11:32:08 -0400

    1.00.1200-alpha2 - Alpha 2 Release

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,8 @@
-import 'bootstrap';
+import { Carousel } from "bootstrap";
 import $ from "jquery";
 import DarkModeSwitch from "./DarkMode.util";
+import AOS from "aos";
+
 
 const CommonFunctions = {
     toggleLoader: () => {
@@ -8,11 +10,30 @@ const CommonFunctions = {
             $("#loader_container").fadeToggle();
         }, 1200);
     },
-    toggleDarkMode: () => {DarkModeSwitch.getInstance()}
+    toggleDarkMode: () => {DarkModeSwitch.getInstance()},
+    carousel: () => {
+        const myCarouselElement = document.querySelector('#carouselExampleCaptions') as HTMLElement
+        const carousel = new Carousel(myCarouselElement, {
+          interval: 5000,
+          wrap: true
+        })
+
+        carousel.cycle();
+    },
+    form: () => {
+        let form = document.querySelector("#formNews") as HTMLFormElement;
+
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            let data = new FormData(form);
+
+
+            alert("Email " + data.get("emailName") + " cadastrado com sucesso!");
+        })
+    }
 }
 
 window.addEventListener("load", () => {
-    let message: string = "HelloWorld";
-
+    AOS.init();
     Object.values(CommonFunctions).map(value => value());
 })
